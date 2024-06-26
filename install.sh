b38e8a4e66a3e8e19b66f802d2c7a6f56bf10849
Elio Merolle <elioamerolle@gmail.com>
2024-06-26 00:13:18 -0400

    fixed some bugs in sh file

--- install.sh
+++ install.sh
@@ -1,20 +1,44 @@
 #!/bin/sh
 
+#!/bin/sh
+
+# Check if conda is installed
 if command -v conda &> /dev/null
 then
-    
-    conda install matplotlib==3.0.3
-    conda install numpy==1.14.2
-    conda install anaconda::scikit-learn
-    conda install tqdm==4.64.1
-    conda install termcolor==1.1.0
+    echo "Conda found. Installing packages with conda..."
+    conda install matplotlib -y
+    conda install numpy==1.14.2 -y
+    conda install anaconda::scikit-learn -y
+    conda install tqdm==4.64.1 -y
+    conda install termcolor==1.1.0 -y
 else
-
-    echo "Conda not found. Installing packages with pip..."
-    pip install matplotlib==3.0.3
-    pip install numpy==1.14.2
-    pip install scikit-learn==0.20.3
-    pip install tqdm==4.64.1
-    pip install termcolor==1.1.0
-
+    # Check if pip is installed
+    if command -v pip &> /dev/null
+    then
+        echo "pip found. Installing packages with pip..."
+        pip install matplotlib
+        pip install numpy==1.14.2
+        pip install scikit-learn==0.20.3
+        pip install sklearn
+	    pip install tqdm==4.64.1
+        pip install termcolor==1.1.0
+    # Check if pip3 is installed
+    elif command -v pip3 &> /dev/null
+    then
+        echo "pip3 found. Installing packages with pip3..."
+        pip3 install matplotlib
+        pip3 install numpy==1.14.2
+        pip3 install scikit-learn==0.20.3
+        pip3 install sklearn
+        pip3 install tqdm==4.64.1
+        pip3 install termcolor==1.1.0
+    else
+        echo "Neither conda, pip, nor pip3 found. Please install one of them first."
+        exit 1
+    fi
 fi
+
+
+
+
+
